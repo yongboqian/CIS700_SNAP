0a4f807fda9cafe92276c34d10d170d86c1d8c23
nstiurca <nstiurca@932b2379-7c70-4f8e-8678-162180ce6ffc>
2015-09-09 00:30:57 +0000

    script to update IP

---
+++ update-ip.bash
@@ -0,0 +1,14 @@
+#/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$PATH
+
+IF=wlan2
+IP_FILE=$HOME/codes/snap/$HOSTNAME-ip.txt
+CUR_IP=`/sbin/ifconfig $IF | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'`
+OLD_IP=`cat $IP_FILE`
+if [ "$OLD_IP" != "$CUR_IP" ]; then
+    echo $CUR_IP > $IP_FILE
+    echo updated ip old: $OLD_IP new: $CUR_IP
+else
+    echo ip is still $CUR_IP
+fi
+    
