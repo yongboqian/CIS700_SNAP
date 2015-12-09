d666a42a6997838c235bc98dfc5ec8d280dea00d
aspecian <aspecian@932b2379-7c70-4f8e-8678-162180ce6ffc>
2015-12-09 09:48:11 +0000

    fixed controller manager so hardware can be controlled. There are some bugs still though.

--- catkin_ws/src/snap_bringup/scripts/duck_hunter.py
+++ catkin_ws/src/snap_bringup/scripts/duck_hunter.py
@@ -41,16 +41,17 @@
 import sys
 import copy
 import rospy
-import moveit_commander
+#import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+from geometry_msgs.msg import Twist
 from std_msgs.msg import String
-from snap_vision_msgs import *
+from snap_vision_msgs.msg import *
 ## END_SUB_TUTORIAL
 
 from std_msgs.msg import String
 
-class move_traj_node():
+class duck_hunter_node():
 
   def __init__(self):
       ## BEGIN_TUTORIAL
@@ -59,10 +60,11 @@ class move_traj_node():
       ## ^^^^^
       ## CALL_SUB_TUTORIAL imports
       ##
-      ## First initialize moveit_commander and rospy.
-      print "============ Starting tutorial setup"
+      ## First initialize  rospy.
+      ## THis requires the move_base to be up and for the trajectory node to be up.
+      print "============ Starting Duck Hunter"
       #import pdb; pdb.set_trace()
-      moveit_commander.roscpp_initialize(sys.argv)
+      #moveit_commander.roscpp_initialize(sys.argv)
       rospy.init_node('duck_hunter',
                       anonymous=True)
       # Execute this function when shutting down
@@ -71,72 +73,29 @@ class move_traj_node():
       # Set the update rate to 1 second by default
       self.rate = rospy.get_param("~node_rate", 10)
       r = rospy.Rate(self.rate)
-
-      ## Instantiate a RobotCommander object.  This object is an interface to
-      ## the robot as a whole.
-      self.robot = moveit_commander.RobotCommander()
-
-      ## Instantiate a PlanningSceneInterface object.  This object is an interface
-      ## to the world surrounding the robot.
-      self.scene = moveit_commander.PlanningSceneInterface()
-
-      ## Instantiate a MoveGroupCommander object.  This object is an interface
-      ## to one group of joints.  In this case the group is the joints in the left
-      ## arm.  This interface can be used to plan and execute motions on the left
-      ## arm.
-      self.group = moveit_commander.MoveGroupCommander("arm")#hi think this needs to match the group published by the initalized moveit node(not in this node. should have been run before this node starts)
-      #import pdb; pdb.set_trace()
-
-      ## We create this DisplayTrajectory publisher which is used below to publish
-      ## trajectories for RVIZ to visualize.
-      self.display_trajectory_publisher = rospy.Publisher(
-                                          '/move_group/display_planned_path',
-                                          moveit_msgs.msg.DisplayTrajectory)
-      self.base_pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)                                    
-      rospy.Subscriber("/simple_move", String, self.moveCb)
-      rospy.Subscriber("/Detection", DetectionStamped, self.DetectionCb)
+      #move base veolicty and arm traj topic 
+      self.base_pub = rospy.Publisher('~/cmd_vel_mux/input/teleop', Twist, queue_size=5)                                    
+      self.arm_pub = rospy.Publisher("~/simple_move", String, queue_size=1)
+      rospy.Subscriber("~/detector_manager_node/detections", DetectionsStamped, self.DetectionCb)
       
-      
-      self.hunting.tolerance = 0.01 # tolerance on how zeroed in the arm needs to be
-      self.hunting.turn_speed = 0.1 # not sure what this is measured in
-      self.hunting.box_width_goal = 100 #ideal size of BB
-      self.hunting.box_height_goal = 100 #ideal size of BB
-      self.hunting.image.width = 640 #width of webcam image
-      self.hunting.image.height = 360 #height of webcam image
-      self.hunting.image.center.x = self.hunting.image.width/2
-      self.hunting.image.center.y = self.hunting.image.height/2
+
+      self.hunting_tolerance = 0.01 # tolerance on how zeroed in the arm needs to be
+      self.hunting_turn_speed = 0.5 # not sure what this is measured in
+      self.hunting_box_width_goal = 100 #ideal size of BB
+      self.hunting_box_height_goal = 100 #ideal size of BB
+      self.hunting_img_width = 640 #width of webcam image
+      self.hunting_img_height = 360 #height of webcam image
+      self.hunting_img_center_x = self.hunting_img_width/2
+      self.hunting_img_center_y = self.hunting_img_height/2
       ## Wait for RVIZ to initialize. This sleep is ONLY to allow Rviz to come up.
-      print "============ Waiting for RVIZ..."
-      rospy.sleep(5)
-      print "============ Starting Duck Hunter "
+      #print "============ Waiting for RVIZ..."
+      #rospy.sleep(5)
+      print "============ Sending arm to Rest Position"
+      data = 'rest' #start cmd for moving arm
+      self.move_arm(data)
+      print "============ Duck Hunter Started"
 
-      ## Getting Basic Information
-      ## ^^^^^^^^^^^^^^^^^^^^^^^^^
-      ##
-      ## We can get the name of the reference frame for this robot
-      print "============ Reference frame: %s" % self.group.get_planning_frame()
-
-      ## We can also print the name of the end-effector link for this group
-      print "============ Reference frame: %s" % self.group.get_end_effector_link()
-
-      ## We can get a list of all the groups in the robot
-      print "============ Robot Groups:"
-      print self.robot.get_group_names()
-
-      ## Sometimes for debugging it is useful to print the entire state of the
-      ## robot.
-      print "============ Printing robot state"
-      print self.robot.get_current_state()
-      print "============"
-      #set goal tolerance to be very large and hopefully solve my problems
-      #self.group.set_goal_orientation_tolerance(3.14)
-      ## Planning to a Pose goal
-      ## ^^^^^^^^^^^^^^^^^^^^^^^
-      ## We can plan a motion for this group to a desired pose for the 
-      ## end-effector
-      #put a while loop here to keep trying to update position or put this as a callback for joystick updates..
-      pose_target = geometry_msgs.msg.Pose()
-      #import pdb; pdb.set_trace()
+     
       # Begin the main loop
       #while not rospy.is_shutdown():
            #r.sleep()
@@ -167,22 +126,23 @@ class move_traj_node():
                 best_conf = data.detections[j].confidence
                 i = j
           #we should have the index of the most duckly object
-          if best_conf != 0:#if we have a duck    
+          if best_conf != 0:#if we have a duck 
+              #print "Quack"   
               #Find the center of the bbox and calculate the offset
-              bbox_center.x = data.detection[i].bbox.x-data.detection[i].bbox.width/2# to be calculated from msg
-              bbox_center.y = data.detection[i].bbox.y-data.detection[i].bbox.height/2
-              center_offset.x = self.hunting.image.center.x-bbox_center.x #^
-              center_offset.y = self.hunting.image.center.y-bbox_center.y#0.0#^
-              bb_width = data.detection[i].bbox.width#100#^
-              bb_height = data.detection[i].bbox.height#100#^
+              bbox_center_x = data.detections[i].bbox.x-data.detections[i].bbox.width/2# to be calculated from msg
+              bbox_center_y = data.detections[i].bbox.y-data.detections[i].bbox.height/2
+              center_offset_x = self.hunting_img_center_x-bbox_center_x #^
+              center_offset_y = self.hunting_img_center_y-bbox_center_y#0.0#^
+              bb_width = data.detections[i].bbox.width#100#^
+              bb_height = data.detections[i].bbox.height#100#^
               
               #estimate how far away robot is based on the expected size of the bounding box
               #function of: width and/or height
-              distEst = bbwidth -  self.hunting.box_width_goal#
+              distEst = bb_width -  self.hunting_box_width_goal#
               
               
               # logic to control how to move base. and then cmd base movement
-              target_aqrd = self.move_base(center_offset.x, distEst) # i think it gets x. might need y instead
+              target_aqrd = self.move_base(center_offset_x, distEst) # i think it gets x. might need y instead
               # check how far away I am
               #TODO
               
@@ -191,154 +151,60 @@ class move_traj_node():
               if target_aqrd: #assume we are within pokeing distance to the 
                 #do arm stuff
                 rospy.loginfo("target aquired")
-                data.data = 'rest' #start cmd for moving arm
-                self.moveCb(data)
+                data = 'rest' #start cmd for moving arm
+                self.move_arm(data)
             
           
           
       
       
-  def move_base(self, rot_offset, dist_off)
+  def move_arm(self,data):
+      ## send command to arm
+      #data.data = 'rest' #start cmd for moving arm
+      print "sending cmd to arm"
+      self.arm_pub.publish(data)
+      
+  def move_base(self, rot_offset, dist_off):
       ## method will handel cmding the base to turn an amount based on how much offset
       ## there is. The offset is directional, the sign defines the rotation, CCW or CW
       
       # I may want to make this a propostional controller with limits
-      turn_speed = self.hunting.turn_speed
+      turn_speed = self.hunting_turn_speed
       #Kp = 1
       #turn_speed = rot_offset*Kp
-      if turn_speed > self.hunting.turn_speed:
-        turn_speed = self.hunting.turn_speed
-      elif turn_speed < self.hunting.turn_speed:
-        turn_speed = -self.hunting.turn_speed
+      if turn_speed > self.hunting_turn_speed:
+        turn_speed = self.hunting_turn_speed
+      elif turn_speed < self.hunting_turn_speed:
+        turn_speed = -self.hunting_turn_speed
       target_aqrd = False  
       
-      if rot_offset > self.hunting.tolerance:
+      if rot_offset < -self.hunting_tolerance:
         #turn CW i think
         control_turn = turn_speed
-        rospy.loginfo("Turning CW")
-      elif rot_offset < -self.hunting.tolerance:
+        #rospy.loginfo("Turning CCW")
+      elif rot_offset > self.hunting_tolerance:
         #turn CCW i think
         control_turn = -turn_speed
-        rospy.loginfo("Turning CCW")
+        #rospy.loginfo("Turning CW")
       else:
         target_aqrd = True
         control_turn = 0
-        
+      
+      #moveing forward code
+      
+      
       twist = Twist()
       twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
       twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = control_turn
       self.base_pub.publish(twist)
       return target_aqrd
-          
-  def moveCb(self, data):
 
-      ## Moving to a pose goal
-      ## ^^^^^^^^^^^^^^^^^^^^^
-      ##
-      ## Moving to a pose goal is similar to the step above
-      ## except we now use the go() function. Note that
-      ## the pose goal we had set earlier is still active 
-      ## and so the robot will try to move to that goal. We will
-      ## not use that function in this tutorial since it is 
-      ## a blocking function and requires a controller to be active
-      ## and report success on execution of a trajectory.
-
-      # Uncomment below line when working with a real robot
-      #group.go(wait=True)
       
-      ## Planning to a joint-space goal 
-      ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      ##
-      ## Let's set a joint space goal and move towards it. 
-      ## First, we will clear the pose target we had just set.
-
-      self.group.clear_pose_targets()
-      #import pdb; pdb.set_trace()
-      ## Then, we will get the current set of joint values for the group
-      group_variable_values = self.group.get_current_joint_values()
-      print "============ Joint values: ", group_variable_values
-
-      ## Now, let's modify one of the joints, plan to the new joint
-      ## space goal and visualize the plan
-      #group_variable_values[0] = 1.0
-      straight_joint_goals = [-1.45,0,0,0,0]
-      rest_joint_goals = [0.0,2.3,-2.0,0,0]
-      cmd = data.data
-      if cmd == str('rest'):
-        group_variable_values = rest_joint_goals
-        rospy.loginfo('Attempting Rest State Plan')
-      elif cmd =='straight':
-        group_variable_values = straight_joint_goals
-        rospy.loginfo('Attempting Straight State Plan')
-      elif cmd == 'wave':
-        rospy.loginfo('Attempting Wave State Plan')
-        num_waves = 1
-        time_wait = 1
-        wave_joint_goals0 = [0.0,1.8,-0.2,-1.5,0]
-        wave_joint_goals1 = [(0.9-1.45),1.8,-0.2,-1.5,0]
-        #wave_joint_goals1[0] =1.6-0.7 
-        wave_joint_goals2 = [(2.3-1.45),1.8,-0.2,-1.5,0]
-        #wave_joint_goals2[0] = 1.6+0.7 
-        #import pdb; pdb.set_trace()
-      else:
-        #do not move
-        group_variable_values=group_variable_values
-        rospy.loginfo('Unrecongized trajectory. Loading current State as goal')
-      #import pdb; pdb.set_trace()
-      if (cmd == 'wave'): # sinlge else for loop wave for time or until something happens?
-        count = 0
-        while (not rospy.is_shutdown()) and count <num_waves :
-            #exicute wave
-            group_variable_values = wave_joint_goals0
-            self.group.set_joint_value_target(group_variable_values)
-            plan2 = self.group.plan()
-            self.group.go(wait=True)
-            #rospy.sleep(time_wait)
-            self.group.clear_pose_targets()
-            
-            
-            group_variable_values = wave_joint_goals1
-            self.group.set_joint_value_target(group_variable_values)
-            plan2 = self.group.plan()
-            self.group.go(wait=True)
-            #rospy.sleep(time_wait)
-            self.group.clear_pose_targets()
-            
-            group_variable_values = wave_joint_goals2
-            self.group.set_joint_value_target(group_variable_values)
-            plan2 = self.group.plan()
-            self.group.go(wait=True)
-            #rospy.sleep(time_wait)
-            self.group.clear_pose_targets()
-            
-            group_variable_values = wave_joint_goals0
-            self.group.set_joint_value_target(group_variable_values)
-            plan2 = self.group.plan()
-            self.group.go(wait=True)
-            #rospy.sleep(time_wait)
-            self.group.clear_pose_targets()
-            count = count + 1
-            
-      else:
-        self.group.set_joint_value_target(group_variable_values)
-        plan2 = self.group.plan()
-        self.group.go(wait=True)
-
-      print "============ Waiting while RVIZ displays plan2 (5sec)..."
-      rospy.sleep(5)
-
-      print "============ exiting movement.."
-
-    # Shutdown function just prints a message.
   def shutdown(self):
       rospy.loginfo("Shutting down moveit Node...")
-      ## Adding/Removing Objects and Attaching/Detaching Objects
-      ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      ## First, we will define the collision object message
-      collision_object = moveit_msgs.msg.CollisionObject()
 
-      ## When finished shut down moveit_commander.
-      moveit_commander.roscpp_shutdown()
+
+      ## When finished shut down 
       twist = Twist()
       twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
       twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
@@ -351,6 +217,6 @@ class move_traj_node():
 
 if __name__=='__main__':
   try:
-    my_node = move_traj_node()
+    my_node = duck_hunter_node()
   except rospy.ROSInterruptException:
     pass
